0f0044fbfdff163fdc579add3898aa0c78c6a242
TheIdanLapid <eontorch@gmail.com>
2018-12-17 14:58:34 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'redux';
-}
+export class AppComponent implements OnInit {
+  constructor(private store: Store<FeedState>) { }
+
+  ngOnInit(): void {
+    const store = this.store;
+    store.subscribe(() => console.log(store.getState()))
+
+    // The only way to mutate the internal state is to dispatch an action.
+    // The actions can be serialized, logged or stored and later replayed.
+    store.dispatch({ type: 'INCREMENT' })
+    // 1
+    store.dispatch({ type: 'INCREMENT' })
+    // 2
+    store.dispatch({ type: 'DECREMENT' })
+    // 1  }
+    title = 'redux';
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreComponent } from './store/store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, { metaReducers })
   ],
   providers: [],
   bootstrap: [AppComponent]
